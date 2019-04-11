d2ecee3322a8683d58e2aae191c59710b2177ef9
Rustam <rustam.shumniy@gmail.com>
2019-04-11 22:37:09 +0300

    Fixed Delete button. --2 hours

--- src/components/contacts/DeleteContact.js
+++ src/components/contacts/DeleteContact.js
@@ -1,24 +1,29 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { deleteContact } from '../../store/actions/contactActions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { deleteContact } from "../../store/actions/contactActions";
 
 class DeleteContact extends Component {
   state = {
     id: this.props.id
   };
 
-  handleSubmit = e => {
+  handleSubmitDelete = e => {
     e.preventDefault();
+    //console.log('EDIT ', this.state.firstName);
     this.props.deleteContact(this.state);
-    this.props.history.push('/');
+    this.props.history.push("/");
   };
 
   render() {
     if (this.props.id) {
       return (
-        <form className='ui form' onSubmit={this.handleSubmit}>
-          <button>DELETE</button>
-        </form>
+        <button
+          className="negative ui button"
+          name={this.props.name}
+          onClick={this.handleSubmitDelete}
+        >
+          DELETE
+        </button>
       );
     } else {
       return <div>Rendering Delete button</div>;
